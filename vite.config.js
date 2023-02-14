868484618af00d424da8b2215ce7448773a1314a
JSun <jasonsutardi@gmail.com>
2023-02-14 17:15:25 +0800

    Update Vite configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/building-sign-extractor/',
+  base: '/building-sign-extractor-app/',
   plugins: [react()]
 })
